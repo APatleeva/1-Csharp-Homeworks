db0e0ee9da91c90ddee5f68ec045b091e1c90ba3
APatleeva <albena.patleeva@gmail.com>
2015-02-27 00:56:13 +0200

    Changed file

--- 1-Intro-programming_Homework/2_PrintMyName/PrintMyName.cs
+++ 1-Intro-programming_Homework/2_PrintMyName/PrintMyName.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 
 class PrintMyName
 {
     static void Main()
     {
-        // Console.WriteLine("My name is Албена");
-        Console.WriteLine("My name is {0}", "Албена");
+        // Console.WriteLine("My name is Nikola");
+        Console.WriteLine("My name is {0}", "Nikola");
     }
 }
 
